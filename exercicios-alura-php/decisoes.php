a80a3a7a3a4d6739ff4c1eb77e51a057a55916b3
Douglas Silva <silvatec.br@gmail.com>
2022-05-31 14:08:37 -0300

    Exercicio sobre decisoes PHP 2.0

--- exercicios-alura-php/decisoes.php
+++ exercicios-alura-php/decisoes.php
@@ -1,17 +1,29 @@
 <?php
 
-$idade = 21;
-$numeroDePessoas = 1;
+$idade = 16;
+$numeroDePessoas = 2;
 
-echo "Você só pode entrar se tiver a partir de 18 anos ou";
+echo "Você só pode entrar se tiver a partir de 18 anos ou ";
 echo "a partir de 16 anos acompanhado" . PHP_EOL;
 
+//Se no bloco de instruções tiver somente uma linha não nescessita colocar as chaves
 if ($idade >= 18) {
-    echo "Você tem $idade anos." . PHP_EOL;
-    echo 'Pode entrar';
+    echo "Você tem $idade anos. Pode entrar sozinho.";
+} else if ($idade >= 16 && $numeroDePessoas > 1) {
+    echo "Você tem $idade anos, está acompanhado(a), então pode entrar.";
 } else {
-    echo "Você só tem $idade anos. Você não pode entrar";
+    echo "Você só tem $idade anos. Você não pode entrar.";
 }
 
-echo  PHP_EOL;
+echo PHP_EOL;
 echo "Até logo!";
+
+/*Além disso, quando há apenas uma linha a ser executada, temos uma outra opção para tomar decisões.
+Se precisamos, por exemplo, atribuir o valor a uma variável nos baseando em alguma condição,
+podemos utilizar o que é chamado de operador ternário. Sua sintaxe é a seguinte:
+$variavel = $condicao ? $valorSeVerdadeiro : $valorSeFalso;*/
+
+/*Exemplo:
+$idade = 15;
+$mensagem = $idade < 18 ? ‘Você é menor de idade’ : ‘Você é maior de idade’;
+echo $mensagem;*/

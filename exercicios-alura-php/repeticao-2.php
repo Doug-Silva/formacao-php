62b35315e65d2006f70ed84ad64c9a42e16dcf0c
Douglas Silva <silvatec.br@gmail.com>
2022-06-05 15:04:56 -0300

    repeticoes-2 update

---
+++ exercicios-alura-php/repeticao-2.php
@@ -0,0 +1,11 @@
+<?php
+
+/*tipos de incrementador
+$contador = $contador + 1;
+$contador += $contador;
+$contador++;*/
+
+//Repetição com for
+for ($contador = 1; $contador <=15; $contador++) {
+    echo "#$contador" . PHP_EOL;
+}
--- exercicios-alura-php/repeticao.php
+++ exercicios-alura-php/repeticao.php
@@ -2,6 +2,7 @@
 
 $contador = 1;
 
+//Repetição com while
 while ($contador <= 15) {
     echo "#$contador" . PHP_EOL;
     $contador = $contador + 1;

0a794edfda05213aedb4307d7caf612abcd0928b
Douglas Silva <silvatec.br@gmail.com>
2022-06-06 00:25:36 -0300

    Break-continue

---
+++ exercicios-alura-php/break-continue.php
@@ -0,0 +1,22 @@
+<?php
+
+for ($contador = 1; $contador <=15; $contador++) {
+    if ($contador == 13) {
+
+        //Com o continue ele pula a linha 13
+        continue;
+    }
+
+    echo "#$contador" . PHP_EOL;
+}
+
+/*for ($contador = 1; $contador <=15; $contador++) {
+    if ($contador == 13) {
+
+        //Com o break ele quebra o loop e exibe somente até a linha 12
+        break;
+    }
+
+    echo "#$contador" . PHP_EOL;
+}*/
+

48ac260449931a4ddf14d343255f965f4effb4f7
Douglas Silva <silvatec.br@gmail.com>
2022-06-09 10:00:35 -0300

    Exercicios php web

--- exercicios-php-avancado/banco.php
+++ exercicios-php-avancado/banco.php
@@ -1,12 +1,11 @@
 <?php
 
-//Inclui como arquivo não importante
-include 'funcoes.php';
-
 //Inclui como arquivo importante, se tiver faltando,
 //já apresenta fatal error e não executa o restante do codigo
+require_once 'funcoes.php';
 
-//require_once 'funcoes.php';
+//Inclui como arquivo não importante
+//include 'funcoes.php';
 
 $contasCorrentes = [
 
@@ -46,11 +45,27 @@ unset($contasCorrentes['123.456.689-11']);
 
 titularComLetrasMaiusculas($contasCorrentes['121.256.789-12']);
 
-foreach ($contasCorrentes as $cpf => $conta) {
+?>
 
-    //Funcao List
-    list('titular' => $titular, 'saldo' => $saldo) = $conta;
-    exibeMensagem(
-        "$cpf $titular $saldo"
-    );
-}
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+
+    <body>
+        <h1>Contas correntes</h1>
+
+        <dl>
+            <?php foreach($contasCorrentes as $cpf => $conta) { ?>
+                <dt>
+                    <h3><?= $conta['titular']; ?> - <?= $cpf; ?></h3>
+                </dt>
+                <dd>Saldo: <?= $conta['saldo']; ?></dd>
+            <?php } ?>
+        </dl>
+    </body>
+</html>

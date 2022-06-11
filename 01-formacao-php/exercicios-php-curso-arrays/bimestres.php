f74d6eda98bad9234822830667e35f062f2dc3b1
Douglas Silva <silvatec.br@gmail.com>
2022-06-11 15:59:59 -0300

    Exercicio bimestres 1.2

---
+++ 01-formacao-php/exercicios-php-curso-arrays/bimestres.php
@@ -0,0 +1,37 @@
+<?php
+
+$notasBimeste1 = [
+    'Vinicius' => 6,
+    'João' => 8,
+    'Ana' => 10,
+    'Roberto' => 7,
+    'Maria' => 9,
+];
+
+$notasBimeste2 = [
+    'Vinicius' => 6,
+    'João' => 8,
+    'Ana' => 9,
+    'Roberto' => 7,
+    'Maria' => 10,
+];
+
+/*
+***********************************************************************************************
+Diferença entre arrays:                                                                       *
+                                                                                              *
+Essa função vai retornar todos os elementos de notasBimeste1 que não estão em notasBimeste2   *
+Obs: Só é levado em consideração o valor                                                      *
+                                                                                              *
+Exemplo:                                                                                      *
+var_dump(array_diff($notasBimeste1, $notasBimeste2));                                         *
+                                                                                              *
+Diferença entre arrays utilizando as chaves e ignorando os valores                            *
+Exemplo2:                                                                                     *
+                                                                                              *
+var_dump(array_diff_key($notasBimeste1, $notasBimeste2));                                     *
+***********************************************************************************************
+*/
+
+//Diferença entre arrays comparando as chaves e os valores
+var_dump(array_diff_assoc($notasBimeste1, $notasBimeste2));

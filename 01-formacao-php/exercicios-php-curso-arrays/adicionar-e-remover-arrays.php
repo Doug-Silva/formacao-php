cc3f04e961140f37ea9a742ae3b1fadd1b22894b
Douglas Silva <silvatec.br@gmail.com>
2022-06-13 12:16:33 -0300

    Exercicio adicionar e remover arrays

---
+++ 01-formacao-php/exercicios-php-curso-arrays/adicionar-e-remover-arrays.php
@@ -0,0 +1,29 @@
+<?php
+
+$alunos2021 = [
+    'Vinicius',
+    'João',
+    'Ana',
+    'Roberto',
+    'Maria',
+];
+
+$novosAlunos = [
+    'Patricia',
+    'Nico',
+    'Diogo',
+    'Daiane',
+];
+
+$alunos2022 = array_merge($alunos2021, $novosAlunos);
+
+//Adicionar elementos no array
+array_push($alunos2022, 'Alice', 'Bob', 'Charlie');
+
+//Adicionar somente um elemento no array
+$alunos2022[] = 'Luiz';
+
+//Adicionar elementos no inicio do array
+array_unshift($alunos2022, 'Stephane', 'Rafaela');
+
+var_dump($alunos2022);

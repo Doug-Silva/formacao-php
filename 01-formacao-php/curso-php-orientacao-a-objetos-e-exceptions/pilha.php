048fa8ab90b5cdf989fd955d72a30d064645d90f
Douglas Silva <silvatec.br@gmail.com>
2022-06-27 11:35:10 -0300

    criando bloco try catch

--- 01-formacao-php/curso-php-orientacao-a-objetos-e-exceptions/pilha.php
+++ 01-formacao-php/curso-php-orientacao-a-objetos-e-exceptions/pilha.php
@@ -5,19 +5,24 @@ function funcao1()
     echo 'Entrei na função 1' . PHP_EOL;
 
     //exemplos de exceções e erros:
+    try {
+        funcao2();
+    } catch (RuntimeException $erro) {
+        echo "Na função 1, eu resolvi o problema da função 2" . PHP_EOL;
+    }
 
-    /*$arrayFixo = new SplFixedArray(2);
-    $arrayFixo[3] = 'Valor';*/
-
-    //$divisao = intdiv(5, 0);
-
-    funcao2();
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
+
+    //erro tratado na função 1
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[4] = 'Valor';
+    //$divisao = intdiv(5, 0);
+
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
